fd00f79f95fd637d866315715f205f0dd3fda25d
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-02-26 07:43:10 +0000

    added is_sad

--- main.py
+++ main.py
@@ -10,16 +10,20 @@ class Cat:
   color = 'Grey'
   age = 3
   name = "Name"
-  
   def __init__(self, name, age):
     self.name = name
     self.age = age
-
   def play_w_human(self, human):
     human.is_sad = False 
+
 
 my_pet = Cat('Zorro', 3)
 me = Person('Dmytro', 180)
 friend = Person('Anton', 178)
 
-print(me.is_sad)
+print(friend.is_sad)
+
+my_pet.play_w_human(friend)
+
+print('Сумний -', friend.is_sad)
+print(friend.is_sad)
