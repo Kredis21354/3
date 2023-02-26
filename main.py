829fda5ad2544a3f31915a7119ab11ee9cc0b6dc
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-02-26 07:20:58 +0000

    added me and anton

---
+++ main.py
@@ -0,0 +1,11 @@
+class Person:
+  height = 170
+  name = "Name"
+  is_sad = True
+  def __init__(self, name, height):
+    self.name =  name
+    self.height = height
+
+
+me = Person('Dmytro', 180)
+friend = Person('Anton', 178)
